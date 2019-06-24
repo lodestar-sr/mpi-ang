34c3e8f2e75ac5397315dc12768be06d64190c9e
Marijo. T <singlejupiter@outlook.com>
2019-06-24 22:13:20 +0200

    working on PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) {
+
+  }
+
+  ngOnInit(): void {
+    this.swUpdate.available.subscribe(() => {
+      if (confirm('New version available. Please load new version.')) {
+        window.location.reload();
+      }
+    });
+  }
+
+}
--- src/app/miles/miles-landing/miles-landing.component.ts
+++ src/app/miles/miles-landing/miles-landing.component.ts
@@ -6,7 +6,6 @@ declare var $: any;
 @Component({
   selector: 'app-miles-landing',
   templateUrl: './miles-landing.component.html',
-  styleUrls: ['./miles-landing.component.scss']
 })
 export class MilesLandingComponent implements OnInit {
 
