ef5b9ba94b40b6237275a2f9850529f4162d5449
Marijo. T <singlejupiter@outlook.com>
2019-06-27 01:05:27 +0200

    add sidebar with updated designs

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,32 +11,14 @@ declare var $: any;
 })
 export class MapComponent implements OnInit {
 
-  public additionalSidebarClass: string;
-
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     fromEvent(window, 'resize').subscribe(event => {
       if (window.innerWidth > 768) {
-        this.additionalSidebarClass = '';
         $('.search-box-bottom').hide();
       } else {
         $('.search-box').hide();
-      }
-    });
-
-    fromEvent<MouseEvent>(document.body, 'click').subscribe((event: any) => {
-      if (window.innerWidth <= 768 &&
-        event.target.id != 'hamburger' &&
-        !event.target.className.includes('fa-compass_regular') &&
-        !event.target.className.includes('fa-university_regular') &&
-        !event.target.className.includes('fa-landmark-alt_regular') &&
-        !event.target.className.includes('fa-city_regular') &&
-        !event.target.className.includes('fa-hotel_regular') &&
-        !event.target.className.includes('nav-link') &&
-        !event.target.className.includes('nav-item')
-      ) {
-        this.additionalSidebarClass = '';
       }
     });
   }
@@ -54,10 +36,5 @@ export class MapComponent implements OnInit {
   }
 
   toggleSidebar() {
-    if (!this.additionalSidebarClass) {
-      this.additionalSidebarClass = 'visible';
-    } else {
-      this.additionalSidebarClass = '';
-    }
   }
 }
--- src/app/map/sidebar/sidebar.component.ts
+++ src/app/map/sidebar/sidebar.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  isSmall: boolean;
+  activated: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.isSmall = false;
+    this.activated = 'HOME';
   }
 
+  switchSidebar(sidebar: any, details: any) {
+    this.isSmall = !this.isSmall;
+    sidebar.style.width = ((this.isSmall ? 56 : 128) + 360) + 'px';
+
+    details.style.left = (this.isSmall ? 56 : 128) + 'px';
+  }
+
+  activateMenu(activatedMenu: string) {
+    this.activated = activatedMenu;
+  }
 }
