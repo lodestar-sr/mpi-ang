5faf228c461c70cc1917b39c0f953a8a986f9cd0
Marijo. T <singlejupiter@outlook.com>
2019-06-28 22:31:51 +0200

    add side details bar

--- src/app/map/sidebar/sidebar.component.ts
+++ src/app/map/sidebar/sidebar.component.ts
@@ -1,30 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {fromEvent} from "rxjs";
+
+declare var $: any;
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
 
   isSmall: boolean;
   activated: string;
-
-  constructor() { }
+  details: any[];
+  selectedDetail: number;
 
   ngOnInit() {
     this.isSmall = false;
     this.activated = 'HOME';
+    this.details = [
+      {
+        title: 'COLORADO',
+        header1: {
+          name: 'COUNTIES',
+          value: 64
+        },
+        header2: {
+          name: 'REPORTING',
+          value: 64
+        },
+        header3: {
+          name: 'ORDINANCES',
+          value: 64
+        },
+        description: '**STATE DATA FEED** This is the area where all data about the selected authority will appear.',
+        updated: '06/25/2019'
+      },
+      {
+        title: 'EAGLE',
+        header1: {
+          name: 'MUNI\'S.',
+          value: 11
+        },
+        header2: {
+          name: 'REPORTING',
+          value: 11
+        },
+        header3: {
+          name: 'ORDINANCES',
+          value: 8
+        },
+        description: '**COUNTY DATA FEED** This is the area where all data about the selected authority will appear.',
+        updated: '06/25/2019'
+      },
+      {
+        title: 'EDWARDS',
+        header1: {
+          name: 'PROPERTIES',
+          value: 9985
+        },
+        header2: {
+          name: 'MANAGED',
+          value: 0
+        },
+        header3: {
+          name: 'ORDINANCES',
+          value: 2
+        },
+        description: '**MUNI DATA FEED** This is the area where all data about the selected authority will appear.',
+        updated: '06/25/2019'
+      },
+    ];
+    this.selectedDetail = 0;
   }
 
-  switchSidebar(sidebar: any, details: any) {
-    this.isSmall = !this.isSmall;
-    sidebar.style.width = ((this.isSmall ? 56 : 128) + 360) + 'px';
+  ngAfterViewInit(): void {
+    this.recalculateHeight(this.selectedDetail);
+  }
 
-    details.style.left = (this.isSmall ? 56 : 128) + 'px';
+  recalculateHeight(no) {
+    const headerHeight = 56;
+    const wrapper = document.querySelector('.accordion-wrapper');
+    if (wrapper) {
+      const wrapHeight = wrapper.clientHeight;
+      const cnt = document.querySelectorAll('.card').length;
+      const freeSpace = wrapHeight - cnt * (headerHeight + 12);
+      const cardbody = document.querySelector('#collapse' + no + ' .card-body');
+      cardbody.style.height = freeSpace + 'px';
+    }
+  }
+
+  switchSidebar(sidebar: any, dtls: any) {
+    this.isSmall = !this.isSmall;
+
+    if (dtls) {
+      dtls.style.left = (this.isSmall ? 56 : 128) + 'px';
+    }
   }
 
   activateMenu(activatedMenu: string) {
     this.activated = activatedMenu;
+  }
+
+  changeStatus(no) {
+    const ele = document.querySelector('#collapse' + no);
+    if (!ele.className.includes('show')) {      // closed status -> open status
+      this.recalculateHeight(no);
+    } else {    // opened status -> close status
+      const cardbody = document.querySelector('#collapse' + no + ' .card-body');
+      cardbody.style.height = '0px';
+    }
   }
 }
