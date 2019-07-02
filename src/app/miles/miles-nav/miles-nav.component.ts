2a8b945f938cf73bd32e16667d84825bd6790271
Marijo. T <singlejupiter@outlook.com>
2019-07-02 22:52:07 +0200

    updated miles navigation

--- src/app/miles/miles-nav/miles-nav.component.ts
+++ src/app/miles/miles-nav/miles-nav.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-miles-nav',
   templateUrl: './miles-nav.component.html',
-  styleUrls: ['./miles-nav.component.scss']
 })
 export class MilesNavComponent implements OnInit {
 
