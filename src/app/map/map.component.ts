ae1a347cba6fa6f7b997cec48d56d3772f0d6aea
Marijo. T <singlejupiter@outlook.com>
2019-07-05 19:07:22 +0200

    updated map component with styles

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,7 +7,6 @@ declare var $: any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
 
