822356a2dbd8c0527f0bd418455d7c9a24143f06
Marijo. T <singlejupiter@outlook.com>
2019-07-12 20:20:04 +0200

    fix attribute panel update issue

--- src/app/map/sidebar/sidebar.component.ts
+++ src/app/map/sidebar/sidebar.component.ts
@@ -21,72 +21,11 @@ export class SidebarComponent implements OnInit, AfterViewInit {
     this.subscription = this.appService.getMessage().subscribe(message => {
       this.zone.run(() => {
         if (message.type == 'state') {
-          this.details = [];
-          this.details[0] = {
-            title: message.name,
-            header1: {
-              name: 'COUNTIES',
-              value: 64
-            },
-            header2: {
-              name: 'REPORTING',
-              value: 64
-            },
-            header3: {
-              name: 'ORDINANCES',
-              value: 64
-            },
-            description: '**STATE DATA FEED** This is the area where all data about the selected authority will appear.',
-            updated: '06/25/2019'
-          };
-          this.selectedDetail = 0;
-          setTimeout(() => this.recalculateHeight(this.selectedDetail), 500);
-          setTimeout(() => this.appService.sendMessage({type: 'resizeMap'}), 500);
+          this.attributeState(message);
         } else if (message.type == 'county') {
-          this.details[1] = {
-            title: message.name,
-            header1: {
-              name: 'MUNI\'S.',
-              value: 11
-            },
-            header2: {
-              name: 'REPORTING',
-              value: 11
-            },
-            header3: {
-              name: 'ORDINANCES',
-              value: 8
-            },
-            description: '**COUNTY DATA FEED** This is the area where all data about the selected authority will appear.',
-            updated: '06/25/2019'
-          };
-          if (this.details.length == 3) {
-            this.details.pop();
-          }
-          this.selectedDetail = 1;
-          setTimeout(() => this.recalculateHeight(this.selectedDetail), 500);
-          setTimeout(() => this.appService.sendMessage({type: 'resizeMap'}), 500);
+          this.attributeCounty(message);
         } else if (message.type == 'town') {
-          this.details[2] = {
-            title: message.name,
-            header1: {
-              name: 'PROPERTIES',
-              value: 9985
-            },
-            header2: {
-              name: 'MANAGED',
-              value: 0
-            },
-            header3: {
-              name: 'ORDINANCES',
-              value: 2
-            },
-            description: '**MUNI DATA FEED** This is the area where all data about the selected authority will appear.',
-            updated: '06/25/2019'
-          };
-          this.selectedDetail = 2;
-          setTimeout(() => this.recalculateHeight(this.selectedDetail), 500);
-          setTimeout(() => this.appService.sendMessage({type: 'resizeMap'}), 500);
+          this.attributeTown(message);
         } else if (message.type == 'gotoHome') {
           this.details = [];
           setTimeout(() => this.appService.sendMessage({type: 'resizeMap'}), 500);
@@ -147,9 +86,90 @@ export class SidebarComponent implements OnInit, AfterViewInit {
     const ele = document.querySelector('#collapse' + no);
     if (!ele.className.includes('show')) {      // closed status -> open status
       this.recalculateHeight(no);
-    } else {    // opened status -> close status
+    } else {                                                // opened status -> close status
       const cardbody: any = document.querySelector('#collapse' + no + ' .card-body');
       cardbody.style.height = '0px';
     }
+  }
+
+  attributeState(message: any) {
+    this.details = [];
+    this.details[0] = {
+      title: message.name,
+      header1: {
+        name: 'COUNTIES',
+        value: 64
+      },
+      header2: {
+        name: 'REPORTING',
+        value: 64
+      },
+      header3: {
+        name: 'ORDINANCES',
+        value: 64
+      },
+      description: '**STATE DATA FEED** This is the area where all data about the selected authority will appear.',
+      updated: '06/25/2019'
+    };
+    this.selectedDetail = 0;
+    setTimeout(() => this.recalculateHeight(this.selectedDetail), 500);
+    setTimeout(() => this.appService.sendMessage({type: 'resizeMap'}), 500);
+  }
+
+  attributeCounty(message: any) {
+    const tmp = Object.assign({}, this.details[0]);
+    this.details = [];
+    this.details[0] = tmp;
+    this.details[1] = {
+      title: message.name,
+      header1: {
+        name: 'MUNI\'S.',
+        value: 11
+      },
+      header2: {
+        name: 'REPORTING',
+        value: 11
+      },
+      header3: {
+        name: 'ORDINANCES',
+        value: 8
+      },
+      description: '**COUNTY DATA FEED** This is the area where all data about the selected authority will appear.',
+      updated: '06/25/2019'
+    };
+    if (this.details.length == 3) {
+      this.details.pop();
+    }
+    this.selectedDetail = 1;
+    setTimeout(() => this.recalculateHeight(this.selectedDetail), 500);
+    setTimeout(() => this.appService.sendMessage({type: 'resizeMap'}), 500);
+  }
+
+  attributeTown(message: any) {
+    const tmp0 = Object.assign({}, this.details[0]);
+    const tmp1 = Object.assign({}, this.details[1]);
+    this.details = [];
+    this.details[0] = tmp0;
+    this.details[1] = tmp1;
+    this.details[2] = {
+      title: message.name,
+      header1: {
+        name: 'PROPERTIES',
+        value: 9985
+      },
+      header2: {
+        name: 'MANAGED',
+        value: 0
+      },
+      header3: {
+        name: 'ORDINANCES',
+        value: 2
+      },
+      description: '**MUNI DATA FEED** This is the area where all data about the selected authority will appear.',
+      updated: '06/25/2019'
+    };
+    this.selectedDetail = 2;
+    setTimeout(() => this.recalculateHeight(this.selectedDetail), 500);
+    setTimeout(() => this.appService.sendMessage({type: 'resizeMap'}), 500);
   }
 }
